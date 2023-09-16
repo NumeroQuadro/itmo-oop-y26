bb2bf1c3e884c6c284b43eb3445c6eefd3d39bca
DIMON3-tech <dimabelunin777@niuitmo.ru>
2023-09-16 20:01:46 +0300

    Initial commit

---
+++ src/Lab1/Program.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Itmo.ObjectOrientedProgramming.Lab1;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("dimon limon");
+    }
+}
