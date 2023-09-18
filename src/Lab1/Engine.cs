54808b90362b70431953ed56843282f33c4742c3
DIMON3-tech <dimabelunin777@niuitmo.ru>
2023-09-19 00:16:45 +0300

    Add EngineClass enum
    Add Engine class
    Add WasteFuelForTransportation method
    add StartEngine method
    add IsEngineHasFuel method
    
    2 warnings

---
+++ src/Lab1/Engine.cs
@@ -0,0 +1,25 @@
+namespace Itmo.ObjectOrientedProgramming.Lab1;
+
+public class Engine
+{
+    public uint FuelConsumptionPerOneAstronomicalUnit { get; init; }
+    public uint CurrentFuelLevel { get; private set; }
+    public uint FuelWasteStart { get; init; }
+    public uint MaximalSpeed { get; init; }
+    public EngineClass EngineClass { get; init; }
+
+    public void WasteFuelForTransportation(uint astronomicalUnits)
+    {
+        CurrentFuelLevel -= astronomicalUnits * FuelConsumptionPerOneAstronomicalUnit;
+    }
+
+    public void StartEngine()
+    {
+        CurrentFuelLevel -= FuelWasteStart;
+    }
+
+    public bool IsEngineHasFuel()
+    {
+        return CurrentFuelLevel != 0;
+    }
+}
---
+++ src/Lab1/EngineClass.cs
@@ -0,0 +1,10 @@
+namespace Itmo.ObjectOrientedProgramming.Lab1;
+
+public enum EngineClass
+{
+    ImpulseClassC,
+    ImpulseClassE,
+    JumpClassAlpha,
+    JumpClassOmega,
+    JumpClassGamma,
+}
