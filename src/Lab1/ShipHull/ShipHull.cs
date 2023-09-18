eb505af5d5586601a19450fccf2a13f8a3bf885e
DIMON3-tech <dimabelunin777@niuitmo.ru>
2023-09-19 01:23:18 +0300

    feat: add ShipHull class feat: add ShipHull.TakeDamage method feat: add ShipHull.CheckBigAsteroidDamage method feat: add ShipHull.CheckSmallAsteroidDamage feat: add ShipHull.IncrementAsteroidCounter method

---
+++ src/Lab1/ShipHull/ShipHull.cs
@@ -0,0 +1,116 @@
+using System;
+using Itmo.ObjectOrientedProgramming.Lab1.Asteroid;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.ShipHull;
+
+public class ShipHull
+{
+    private uint _smallAsteroidCounter;
+    private uint _bigAsteroidCounter;
+    private bool _isDestroyed;
+    private ShipHullClass _shipHullClass;
+    private Engine.Engine _engine;
+
+    public ShipHull(ShipHullClass shipHullClass, Engine.Engine engine)
+    {
+        if (engine == null)
+        {
+            throw new ArgumentException("Engine is a null!");
+        }
+
+        _engine = engine;
+
+        _smallAsteroidCounter = 0;
+        _bigAsteroidCounter = 0;
+
+        _isDestroyed = false;
+        _shipHullClass = shipHullClass;
+    }
+
+    public void TakeDamage(AsteroidClass asteroidClass)
+    {
+        const uint oneAsteroidNumber = 1;
+
+        IncrementAsteroidCounter(incrementNumber: oneAsteroidNumber, asteroidClass: asteroidClass);
+    }
+
+    private void CheckBigAsteroidDamage()
+    {
+        switch (_shipHullClass)
+        {
+            case ShipHullClass.Class2:
+                const uint class2HullCapacityBigAsteroids = 2;
+
+                if (_bigAsteroidCounter == class2HullCapacityBigAsteroids)
+                {
+                    _isDestroyed = true;
+                }
+
+                break;
+            case ShipHullClass.Class3:
+                const uint class3HullCapacityBigAsteroids = 5;
+
+                if (_bigAsteroidCounter == class3HullCapacityBigAsteroids)
+                {
+                    _isDestroyed = true;
+                }
+
+                break;
+
+            default:
+                _isDestroyed = false;
+                break;
+        }
+    }
+
+    private void CheckSmallAsteroidDamage()
+    {
+        switch (_shipHullClass)
+        {
+            case ShipHullClass.Class1:
+                const uint class1HullCapacitySmallAsteroids = 1;
+
+                if (_smallAsteroidCounter == class1HullCapacitySmallAsteroids)
+                {
+                    _isDestroyed = true;
+                }
+
+                break;
+            case ShipHullClass.Class2:
+                const uint class2HullCapacitySmallAsteroids = 5;
+
+                if (_smallAsteroidCounter == class2HullCapacitySmallAsteroids)
+                {
+                    _isDestroyed = true;
+                }
+
+                break;
+            case ShipHullClass.Class3:
+                const uint class3HullCapacitySmallAsteroids = 20;
+
+                if (_smallAsteroidCounter == class3HullCapacitySmallAsteroids)
+                {
+                    _isDestroyed = true;
+                }
+
+                break;
+        }
+    }
+
+    private void IncrementAsteroidCounter(uint incrementNumber, AsteroidClass asteroidClass)
+    {
+        if (!_isDestroyed)
+        {
+            if (asteroidClass == AsteroidClass.Big)
+            {
+                _bigAsteroidCounter += incrementNumber;
+                CheckBigAsteroidDamage();
+            }
+            else
+            {
+                _smallAsteroidCounter += incrementNumber;
+                CheckSmallAsteroidDamage();
+            }
+        }
+    }
+}
---
+++ src/Lab1/ShipHull/ShipHullClass.cs
@@ -0,0 +1,8 @@
+namespace Itmo.ObjectOrientedProgramming.Lab1.ShipHull;
+
+public enum ShipHullClass
+{
+    Class1,
+    Class2,
+    Class3,
+}
