878910c2aaac1788868c1bf2e80282b3126ed131
DIMON3-tech <dimabelunin777@niuitmo.ru>
2023-09-21 19:03:03 +0300

    forget to mark Protection.cs

--- src/Lab1/Environment/Ship/Protection/Protection.cs
+++ src/Lab1/Environment/Ship/Protection/Protection.cs
@@ -2,35 +2,7 @@ namespace Itmo.ObjectOrientedProgramming.Lab1.Environment.Ship.Protection;
 
 public abstract class Protection
 {
-    private readonly ProtectionType _protectionType;
+    public abstract void TakeDamage();
 
-    protected Protection(ProtectionType protectionType)
-    {
-        _protectionType = protectionType;
-    }
-
-    protected uint AsteroidCounter { get; set; }
-    protected uint MeteorCounter { get; set; }
-
-    protected abstract void AssignNumericalConstantCharacteristics(ProtectionType protectionType);
-    protected void DecrementAsteroidCounter()
-    {
-        if (IsProtectionActive())
-        {
-            --AsteroidCounter;
-        }
-    }
-
-    protected void DecrementMeteorCounter()
-    {
-        if (IsProtectionActive())
-        {
-            --MeteorCounter;
-        }
-    }
-
-    protected bool IsProtectionActive()
-    {
-        return MeteorCounter != 0 & AsteroidCounter != 0;
-    }
+    public abstract ProtectionCondition ProtectionCondition();
 }
