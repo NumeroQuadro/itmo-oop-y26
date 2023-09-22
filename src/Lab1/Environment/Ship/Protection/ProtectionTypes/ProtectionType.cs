fcc082a2f2877766f24661c8861073b4e95b46d7
DIMON3-tech <dimabelunin777@niuitmo.ru>
2023-09-22 16:31:08 +0300

    refactor: forget to commit ProtectionType.cs

--- src/Lab1/Environment/Ship/Protection/ProtectionTypes/ProtectionType.cs
+++ src/Lab1/Environment/Ship/Protection/ProtectionTypes/ProtectionType.cs
@@ -1,9 +1,3 @@
 namespace Itmo.ObjectOrientedProgramming.Lab1.Environment.Ship.Protection.ProtectionTypes;
 
-public enum ProtectionType
-{
-    Class1,
-    Class2,
-    Class3,
-    NoProtection,
-}
+public abstract record ProtectionType;
