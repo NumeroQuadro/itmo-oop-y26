bf38acaf796c57a3b7c30ab63f884d57d3754f9a
DIMON3-tech <dimabelunin777@niuitmo.ru>
2023-11-09 12:16:06 +0300

    add UserAdressee (proxy under User)

--- src/Lab3/DisplayAddresses/DisplayAdressee.cs
+++ src/Lab3/DisplayAddresses/DisplayAdressee.cs
@@ -2,7 +2,7 @@ using System;
 using Itmo.ObjectOrientedProgramming.Lab3.Displays;
 using Itmo.ObjectOrientedProgramming.Lab3.Messages;
 
-namespace Itmo.ObjectOrientedProgramming.Lab3;
+namespace Itmo.ObjectOrientedProgramming.Lab3.DisplayAddresses;
 
 public class DisplayAdressee : IAdressee
 {
---
+++ src/Lab3/UserAdressees/UserAdressee.cs
@@ -0,0 +1,15 @@
+using System;
+using Itmo.ObjectOrientedProgramming.Lab3.Messages;
+using Itmo.ObjectOrientedProgramming.Lab3.Users;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.UserAdresses;
+
+public class UserAdressee : IAdressee
+{
+    private readonly Lazy<User> _user = new Lazy<User>(() => new User());
+
+    public void GetMessage(Message message)
+    {
+        _user.Value.GetMessage(message);
+    }
+}
